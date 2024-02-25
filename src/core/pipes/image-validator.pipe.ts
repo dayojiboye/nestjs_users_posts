7814dde4e11ad3b83bb31deb1dfb1609169a51d5
dayojiboye <my@personal>
2024-02-25 08:57:51 +0100

    fix: minor fixes

--- src/core/pipes/image-validator.pipe.ts
+++ src/core/pipes/image-validator.pipe.ts
@@ -1,9 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  PipeTransform,
-  UnprocessableEntityException,
-} from '@nestjs/common';
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 import { extname } from 'path';
 
 export function validateImage(allowedExtensions: string[]): PipeTransform {
@@ -16,19 +11,13 @@ export class ParseFilePipeDocument implements PipeTransform {
 
   transform(file: Express.Multer.File): Express.Multer.File {
     if (!file) {
-      throw new UnprocessableEntityException({
-        message: 'Image field must not be empty',
-        error: 'Unprocessable Entity',
-        statusCode: 422,
-      });
+      throw new BadRequestException('Image field must not be empty');
     }
 
     if (!file.mimetype.startsWith('image')) {
-      throw new UnprocessableEntityException({
-        message: 'Sorry, this file is not an image. Please try again',
-        error: 'Unprocessable Entity',
-        statusCode: 422,
-      });
+      throw new BadRequestException(
+        'Sorry, this file is not an image. Please try again',
+      );
     }
 
     const extension = extname(file.originalname);
@@ -37,11 +26,9 @@ export class ParseFilePipeDocument implements PipeTransform {
     }
 
     if (file.size > 3000000) {
-      throw new UnprocessableEntityException({
-        message: 'Please upload an image size not more than 3MB',
-        error: 'Unprocessable Entity',
-        statusCode: 422,
-      });
+      throw new BadRequestException(
+        'Please upload an image size not more than 3MB',
+      );
     }
 
     return file;
